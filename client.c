1cf2ac5dd0d76ed88f4ba43d5a9b129f2cefc510
Chaillet Maxime <chaillet.job@gmail.com>
2017-02-11 16:49:00 +0100

    client envoi parametre de la partie ok

--- client.c
+++ client.c
@@ -24,92 +24,122 @@
 #define SERVICE_DEFAUT "1111" //1111 initialement
 #define SERVEUR_DEFAUT "localhost"
 
-void client_appli (char *serveur, char *service);
+int masocket;
+
+
+void client_appli(char *serveur, char *service);
 
 
 /*****************************************************************************/
 /*--------------- programme client -----------------------*/
 
-int main(int argc, char *argv[])
-{
+/**
+ * Creation d'une connexion
+ * @param serveur : 
+ * @param service : le port utilisé
+ * @return : code d'erreur: 0=succes, 1=erreur
+ */
+int connexion(char *serveur, char *service) {
+    //declaration de types
+    struct sockaddr_in *p_sockaddr_locale;
+    struct sockaddr_in *p_sockaddr_distante;
 
-	char *serveur= SERVEUR_DEFAUT; /* serveur par defaut */
-	char *service= SERVICE_DEFAUT; /* numero de service par defaut du serveur (no de port) */
+
+    // création d'une socket,
+    // la structure est crée et associée à un file descriptor
+    // renvoi le file descriptor
+    masocket = h_socket(AF_INET, SOCK_STREAM);
+    if (masocket < 0) {
+        printf("Echec création de socket");
+        return 1;
+    } else {
+        printf("Socket créée avec succès. id= %d\n", masocket);
+    }
+
+    // Remplissage d'une structure sockaddr_in pour ip/port du serveur local
+    // le system choisi le port
+    // nom du pointeur: p_sockaddr_local
+    adr_socket(0, serveur, SOCK_STREAM, &p_sockaddr_locale);
+    h_bind(masocket, p_sockaddr_locale);
+    // Remplissage d'une structure sockaddr_in pour ip/port du serveur distant
+    adr_socket(service, serveur, SOCK_STREAM, &p_sockaddr_distante);
+    h_connect(masocket, p_sockaddr_distante);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+    char *serveur = SERVEUR_DEFAUT; /* serveur par defaut */
+    char *service = SERVICE_DEFAUT; /* numero de service par defaut du serveur (no de port) */
+
+    /* Permet de passer un nombre de parametre variable a l'executable */
+    switch (argc) {
+        case 1: /* arguments par defaut */
+            printf("serveur par defaut: %s\n", serveur);
+            printf("service par defaut: %s\n", service);
+            break;
+        case 2: /* serveur renseigne  */
+            serveur = argv[1];
+
+            printf("service par defaut: %s\n", service);
+            break;
+        case 3: /* serveur, service renseignes */
+            serveur = argv[1];
+            service = argv[2];
+            break;
+        default:
+            printf("Usage:client serveur(nom ou @IP)  service (nom ou port) \n");
+            exit(1);
+    }
 
 
-	/* Permet de passer un nombre de parametre variable a l'executable */
-	switch(argc)
-	{
- 	case 1 :		/* arguments par defaut */
-		  printf("serveur par defaut: %s\n",serveur);
-		  printf("service par defaut: %s\n",service);
-		  break;
-  	case 2 :		/* serveur renseigne  */
-		  serveur=argv[1];
+    /* serveur est le nom (ou l'adresse IP) auquel le client va acceder */
+    /* service le numero de port sur le serveur correspondant au  */
+    /* service desire par le client */
 
-		  printf("service par defaut: %s\n",service);
-		  break;
-  	case 3 :		/* serveur, service renseignes */
-		  serveur=argv[1];
-		  service=argv[2];
-		  break;
-    default:
-		  printf("Usage:client serveur(nom ou @IP)  service (nom ou port) \n");
-		  exit(1);
-	}
+    client_appli(serveur, service);
+}
 
-	/* serveur est le nom (ou l'adresse IP) auquel le client va acceder */
-	/* service le numero de port sur le serveur correspondant au  */
-	/* service desire par le client */
+int choixNiveauJeu(int *level) {
+    // choix du niveau de jeu
+    char chaine[1];
+    printf("Veuillez choisir le niveau de jeu (entre 1 et 8) : ");
+    scanf("%d", level);
+    if (*level == 0) {
+        printf("Valeur entrée incorrecte.");
+    } else {
+        sprintf(chaine, "%d", *level);
+        envoieTCP(chaine, 1);
+    }
+}
 
-	client_appli(serveur,service);
+int envoieTCP(char data[], int nbData) {
+    printf("ecriture de 1 octets dans le buffer d'emission.\n");
+    h_writes(masocket, data, nbData);
+    printf("le buffer d'emission qui a été envoyé contient %s", data);
 }
 
 /*****************************************************************************/
-void client_appli (char *serveur,char *service)
+void client_appli(char *serveur, char *service)
 
-/* procedure correspondant au traitement du client de votre application */
-
-{
-//declaration de types
-struct sockaddr_in *p_sockaddr_locale;
-struct sockaddr_in *p_sockaddr_distante;
-
-
-// création d'une socket,
-// la structure est crée et associée à un file descriptor
-// renvoi le file descriptor
-int socket = h_socket(AF_INET,SOCK_STREAM);
-if (socket < 0) {
-	printf("Echec création de socket");
-	} else {
-        printf("Socket créée avec succès. id= %d\n",socket);
-        }
-
-// Remplissage d'une structure sockaddr_in pour ip/port du serveur local
-// le system choisi le port
-// nom du pointeur: p_sockaddr_local
-adr_socket(0, serveur, SOCK_STREAM, &p_sockaddr_locale);
-h_bind(socket, p_sockaddr_locale);
-// Remplissage d'une structure sockaddr_in pour ip/port du serveur distant
-// nom du pointeur: p_sockaddr_distant
-adr_socket(service, serveur, SOCK_STREAM, &p_sockaddr_distante);
-h_connect(socket, p_sockaddr_distante);
-//sleep(200);
-
-char buf_emission[1];
-buf_emission[0]= 'A';
-printf("ecriture de 1 octets dans le buffer d'emission.\n");
-h_writes(socket, buf_emission, 1);
-printf("le buffer d'emission qui a été envoyé contient %s", buf_emission);
-
-sleep(200);
-
-//h_bind(socket, p_sockaddr_local);
-//
+/* procedure correspondant au traitement du client de votre application */ {
+    // etablissement de la connexion
+    int connecte = connexion(serveur, service);
+    if (connecte != 0) {
+        printf("Désolé, Impossible de jouer. Probleme lors de l'établissement de la connexion. \n");
+    } else {
+        int level;
+        choixNiveauJeu(&level);
 
 
-// Code du jeu
+
+        sleep(20);
+    }
+    //h_bind(socket, p_sockaddr_local);
+    //
+
+
+    // Code du jeu
 
 }
 /*****************************************************************************/
---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main (){
+char c = '3';
+int i =4;
+char convi;
+convi = (char)i;
+printf("c vaut %c", c);
+return 0;
+}
