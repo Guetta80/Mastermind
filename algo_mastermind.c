0de632bcd8113a75e7d7e432bbcc99ce9009a3b2
Chaillet Maxime <chaillet.job@gmail.com>
2017-02-11 10:56:35 +0100

    Algo de creation des indicateurs ok

--- algo_mastermind.c
+++ algo_mastermind.c
@@ -40,29 +40,29 @@ void genere(int T[], unsigned int l) {
 
 int CouleurVersEntier(char *coul) {
 
-    if (strcmp(coul, "Rouge")== 0) {
+    if (strcmp(coul, "Rouge") == 0) {
         return 0;
     } else {
-        if (strcmp(coul, "Jaune")== 0) {
+        if (strcmp(coul, "Jaune") == 0) {
             return 1;
         } else {
-            if (strcmp(coul, "Vert")== 0) {
+            if (strcmp(coul, "Vert") == 0) {
                 return 2;
             } else {
-                if (strcmp(coul, "Bleu")== 0) {
+                if (strcmp(coul, "Bleu") == 0) {
                     return 3;
                 } else {
-                    if (strcmp(coul, "Orange")== 0) {
+                    if (strcmp(coul, "Orange") == 0) {
                         return 4;
                     } else {
-                        if (strcmp(coul, "Blanc")== 0) {
+                        if (strcmp(coul, "Blanc") == 0) {
                             return 5;
                         } else {
-                            if (strcmp(coul, "Violet")== 0) {
+                            if (strcmp(coul, "Violet") == 0) {
                                 return 6;
-                            } else return 7;
-
-
+                            } else {
+                                return 7;
+                            }
                         }
                     }
                 }
@@ -71,16 +71,115 @@ int CouleurVersEntier(char *coul) {
     }
 }
 
+int recherche(int T[], unsigned int l, unsigned x) {
+    /* Recherche de l'entier x dans le tableau T */
+
+    int existe;
+
+    unsigned int i;
+
+    i = 0;
+    while (i < l && T[i] != x) {
+        i = i + 1;
+    };
+
+    existe = 0;
+
+    if (i < l) {
+        existe = 1;
+    };
+
+    return existe;
+}
+
+int rechercheP(int T[], unsigned int l, unsigned x) {
+    /* Recherche de la position de l'entier x dans le tableau T */
+
+    unsigned int i;
+
+    int position = 0;
+
+    i = 0;
+    while (i < l && T[i] != x) {
+        i = i + 1;
+    };
+
+    if (i < l) {
+        position = i;
+    };
+
+    return position;
+}
+
+/* compare la proposition avec la combinaison secrète et renvoie un tableau d'indicateurs */
+
+void Reponse(int T_prop[], int T_partie[], int T_indic[], int level) {
+
+    int Temp_prop[level];
+    int Temp_partie[level];
+
+    unsigned int i;
+
+    /* les temporaires */
+
+    for (i = 0; i < level; i++) {
+
+        Temp_partie[i] = T_partie[i];
+        Temp_prop[i] = T_prop[i];
+
+    }
+
+    /* Analyse des jetons bien placés */
+
+    for (i = 0; i < level; i++) {
+        if (Temp_prop[i] == Temp_partie[i]) {
+            T_indic[i] = 2;
+            Temp_prop[i] = -1;
+            Temp_partie[i] = -1;
+        }
+    }
+
+    /* Analyse des jetons non présents */
+
+    for (i = 0; i < level; i++) {
+        if (Temp_prop[i] != -1) {
+            /* Recherche */
+            int estPresent;
+
+            estPresent = recherche(Temp_partie, level, Temp_prop[i]);
+            if (estPresent == 0) {
+                Temp_prop[i] = -1;
+            }
+        }
+    }
+
+    /* Analyse des jetons mal placés */
+
+    for (i = 0; i < level; i++) {
+        if (Temp_prop[i] != -1) {
+            /* Recherche */
+            int estPresent;
+
+            estPresent = recherche(Temp_partie, level, Temp_prop[i]);
+            if (estPresent == 1) {
+                T_indic[i] = 1;
+                Temp_partie[rechercheP(Temp_partie, level, Temp_prop[i])] = -1;
+            }
+        }
+    }
+
+}
+
 void proposition(int T[], unsigned int niv) {
 
-    char boule[10];
+    char Boule[10];
 
     unsigned int i;
     for (i = 0; i < niv; i++) {
 
-        printf("Proposer une couleur (rang %d)", i);
-        scanf("%s", boule);
-        T[i] = CouleurVersEntier(boule);
+        printf("Proposer une couleur (rang %d) : ", i);
+        scanf("%s", Boule);
+        T[i] = CouleurVersEntier(Boule);
     }
 }
 
@@ -89,31 +188,46 @@ int main() {
     int level;
 
     // choix du niveau de jeu
-    printf("Veuillez choisir le niveau de jeu (entre 1 et 8): ");
+    printf("Veuillez choisir le niveau de jeu (entre 1 et 8) : ");
     scanf("%d", &level);
 
     // creation combinaison à trouver aléatoirement
     int Tab_partie[level];
     srandom(time(NULL)); /* germe pour la suite pseudo-aleatoire */
     genere(Tab_partie, level);
+
+    printf("Rouge : 0, Jaune : 1, Vert : 2, Bleu : 3, Orange : 4, Blanc : 5, Violet : 6, Fushia : 7\n");
+
+    printf("Code secret : ");
     affiche(Tab_partie, level);
 
     // boucle de jeu
 
     int Tab_proposition[level];
 
-    int x = 1;
-    while (x == 1) {
+    //int x =1;
+    //while (x==1) {
 
-        // demander au joueur de faire une proposition
+    // demander au joueur de faire une proposition
+    proposition(Tab_proposition, level);
+    printf("Proposition du joueur : ");
+    affiche(Tab_proposition, level);
+    //}
 
+    int Indic[level];
+    int i;
+    for (i = 0; i < level; i++) {
 
-        proposition(Tab_proposition, level);
-        affiche(Tab_proposition, level) ;
-        
-        //sortie bouvle
-        x=0;
+        Indic[i] = 0;
     }
+
+    Reponse(Tab_proposition, Tab_partie, Indic, level);
+
+    printf("Indicateurs : ");
+    affiche(Indic, level);
+    printf("2 : bonne place, 1 : existe mais mal place, 0 : n'existe pas\n");
+
+
 
     return 0;
 
